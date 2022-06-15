cf72e3b8529a68f904d44b353a7a00ec619317b2
Kaaveh <Kaaveh@pm.me>
2022-06-15 17:24:08 +0430

    Implement get json news use case

---
+++ app/src/main/java/ir/kaaveh/baadbaadaknews/domain/usecase/GetJsonNewsUseCase.kt
@@ -0,0 +1,28 @@
+package ir.kaaveh.baadbaadaknews.domain.usecase
+
+import ir.kaaveh.baadbaadaknews.common.Resource
+import ir.kaaveh.baadbaadaknews.domain.model.Article
+import ir.kaaveh.baadbaadaknews.domain.repository.JsonNewsRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+
+class GetJsonNewsUseCase @Inject constructor(
+    private val repository: JsonNewsRepository,
+) {
+
+    operator fun invoke(): Flow<Resource<List<Article>>> = flow {
+        try {
+            emit(Resource.Loading())
+            val articles = repository.getJsonNews()
+            emit(Resource.Success(articles))
+        } catch (e: HttpException) {
+            emit(Resource.Error(e))
+        } catch (e: IOException) {
+            emit(Resource.Error(e))
+        }
+    }
+
+}
