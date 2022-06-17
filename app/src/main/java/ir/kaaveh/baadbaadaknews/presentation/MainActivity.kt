fd695ea670eefb33eaa4680ebcf2d6a6a0047adb
Kaaveh <Kaaveh@pm.me>
2022-06-17 20:23:35 +0430

    Setup NewsListScreen to NewsDetailScreen navigation

--- app/src/main/java/ir/kaaveh/baadbaadaknews/presentation/MainActivity.kt
+++ app/src/main/java/ir/kaaveh/baadbaadaknews/presentation/MainActivity.kt
@@ -3,12 +3,8 @@ package ir.kaaveh.baadbaadaknews.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import com.ramcosta.composedestinations.DestinationsNavHost
 import dagger.hilt.android.AndroidEntryPoint
-import ir.kaaveh.baadbaadaknews.presentation.news_list.NavGraphs
 import ir.kaaveh.baadbaadaknews.presentation.ui.theme.BaadBaadakNewsTheme
 
 @AndroidEntryPoint
@@ -20,18 +16,5 @@ class MainActivity : ComponentActivity() {
                 DestinationsNavHost(navGraph = NavGraphs.root)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    BaadBaadakNewsTheme {
-        Greeting("Android")
     }
 }
